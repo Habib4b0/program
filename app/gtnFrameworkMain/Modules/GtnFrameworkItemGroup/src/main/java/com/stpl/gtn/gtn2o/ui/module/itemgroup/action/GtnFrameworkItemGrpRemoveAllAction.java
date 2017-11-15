1b8edb5e89d9325a523993fb99e0820244d40b97
Deepika KrishnaKumar <deepika.krishnakumar@sysbiz.com>
2017-11-15 12:50:35 +0530

    ALG-2759 - Issue Fixed

--- app/gtnFrameworkMain/Modules/GtnFrameworkItemGroup/src/main/java/com/stpl/gtn/gtn2o/ui/module/itemgroup/action/GtnFrameworkItemGrpRemoveAllAction.java
+++ app/gtnFrameworkMain/Modules/GtnFrameworkItemGroup/src/main/java/com/stpl/gtn/gtn2o/ui/module/itemgroup/action/GtnFrameworkItemGrpRemoveAllAction.java
@@ -6,10 +6,14 @@ import java.util.List;
 import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameWorkAction;
 import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameWorkActionConfig;
 import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameworkActionShareable;
+import com.stpl.gtn.gtn2o.ui.framework.action.executor.GtnUIFrameworkActionExecutor;
 import com.stpl.gtn.gtn2o.ui.framework.component.table.pagedtable.GtnUIFrameworkPagedTableLogic;
 import com.stpl.gtn.gtn2o.ui.framework.engine.GtnUIFrameworkGlobalUI;
 import com.stpl.gtn.gtn2o.ui.framework.engine.base.GtnUIFrameworkDynamicClass;
+import com.stpl.gtn.gtn2o.ui.framework.type.GtnUIFrameworkActionType;
+import com.stpl.gtn.gtn2o.ui.module.itemgroup.constants.GtnFrameworkItemGrpStringContants;
 import com.stpl.gtn.gtn2o.ws.GtnUIFrameworkWebServiceClient;
+import com.stpl.gtn.gtn2o.ws.bean.GtnWsRecordBean;
 import com.stpl.gtn.gtn2o.ws.components.GtnWebServiceSearchCriteria;
 import com.stpl.gtn.gtn2o.ws.constants.common.GtnFrameworkCommonConstants;
 import com.stpl.gtn.gtn2o.ws.exception.GtnFrameworkGeneralException;
@@ -35,9 +39,26 @@ public class GtnFrameworkItemGrpRemoveAllAction
 			throws GtnFrameworkGeneralException {
 		GtnWsSearchRequest sRequest = new GtnWsSearchRequest();
 		GtnWsGeneralRequest generalWSRequest = new GtnWsGeneralRequest();
+		List<GtnWsRecordBean> selectedItems = GtnUIFrameworkGlobalUI
+				.getVaadinBaseComponent(GtnFrameworkCommonConstants.ITEM_GRP_SELECTED_RESULT_TABLE)
+				.getItemsFromDataTable();
+
+		if (selectedItems == null || selectedItems.isEmpty()) {
+			GtnUIFrameWorkActionConfig alertRemoveAllActionConfig = new GtnUIFrameWorkActionConfig();
+			alertRemoveAllActionConfig.setActionType(GtnUIFrameworkActionType.ALERT_ACTION);
+
+			List<Object> alertRemoveAllParamsList = new ArrayList<>();
+			alertRemoveAllParamsList.add(GtnFrameworkItemGrpStringContants.GTN_ITEM_GRP_VALIDATION_ADD_MSG);
+			alertRemoveAllParamsList.add(GtnFrameworkItemGrpStringContants.GTN_ITEM_GRP_VALIDATION_REMOVE_MSG_BODY);
+
+			alertRemoveAllActionConfig.setActionParameterList(alertRemoveAllParamsList);
+			GtnUIFrameworkActionExecutor.executeSingleAction(componentId, alertRemoveAllActionConfig);
+		}
+
 		sRequest.setGtnWebServiceSearchCriteriaList(getSearchCriteriaList());
 		GtnUIFrameworkWebserviceRequest request = new GtnUIFrameworkWebserviceRequest();
 		request.setGtnWsSearchRequest(sRequest);
+
 		try {
 			generalWSRequest.setUserId(GtnUIFrameworkGlobalUI.getCurrentUser());
 			generalWSRequest.setSessionId(String.valueOf(GtnUIFrameworkGlobalUI.getSessionProperty("sessionId")));
