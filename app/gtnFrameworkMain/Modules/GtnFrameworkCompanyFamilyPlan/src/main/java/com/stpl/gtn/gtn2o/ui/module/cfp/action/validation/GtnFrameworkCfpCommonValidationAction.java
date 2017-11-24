2b515428bb1c7e6d666e7b010ac808f0a570c998
Deepika KrishnaKumar <deepika.krishnakumar@sysbiz.com>
2017-11-24 19:15:45 +0530

    ALG-2882 - Issue Fixed

--- app/gtnFrameworkMain/Modules/GtnFrameworkCompanyFamilyPlan/src/main/java/com/stpl/gtn/gtn2o/ui/module/cfp/action/validation/GtnFrameworkCfpCommonValidationAction.java
+++ app/gtnFrameworkMain/Modules/GtnFrameworkCompanyFamilyPlan/src/main/java/com/stpl/gtn/gtn2o/ui/module/cfp/action/validation/GtnFrameworkCfpCommonValidationAction.java
@@ -16,6 +16,7 @@ import com.stpl.gtn.gtn2o.ws.logger.GtnWSLogger;
 import com.stpl.gtn.gtn2o.ws.request.GtnUIFrameworkWebserviceRequest;
 import com.stpl.gtn.gtn2o.ws.request.GtnWsGeneralRequest;
 import com.stpl.gtn.gtn2o.ws.response.GtnUIFrameworkWebserviceResponse;
+import java.util.Date;
 
 public class GtnFrameworkCfpCommonValidationAction
 		implements GtnUIFrameWorkAction, GtnUIFrameworkActionShareable, GtnUIFrameworkDynamicClass {
@@ -36,11 +37,17 @@ public class GtnFrameworkCfpCommonValidationAction
 		gtnWsGeneralRequest.setUserId(String.valueOf(GtnUIFrameworkGlobalUI.getSessionProperty("userId")));
 		gtnWsGeneralRequest.setSessionId(String.valueOf(GtnUIFrameworkGlobalUI.getSessionProperty("sessionId")));
 		request.setGtnWsGeneralRequest(gtnWsGeneralRequest);
+                 Date cfpStartDate = GtnUIFrameworkGlobalUI.getVaadinBaseComponent("cfpInformationCFPStartDate")
+				.getDateFromDateField();
+		Date cfpEndDate = GtnUIFrameworkGlobalUI.getVaadinBaseComponent("cfpInformationCFPEndDate")
+				.getDateFromDateField();
 		GtnUIFrameworkWebserviceResponse response = new GtnUIFrameworkWebServiceClient().callGtnWebServiceUrl(
 				GtnWsCFamilyPlanContants.GTN_WS_CFP_SERVICE
 						+ GtnWsCFamilyPlanContants.GTN_WS_CFP_COMMON_VALIDATION_SERVICE,
 				request, GtnUIFrameworkGlobalUI.getGtnWsSecurityToken());
 		GtnCFamilyPlanValidationBean validationBean = response.getGtnWsCfpReponse().getGtnCFamilyPlanValidationBean();
+               
+                validateDateEqualOrGreater(cfpStartDate,cfpEndDate,componentId);
 		validateAtleastOneCompanySelected(componentId, validationBean);
 		validateStartDate(componentId, validationBean);
 		validateStatus(componentId, validationBean);
@@ -130,6 +137,18 @@ public class GtnFrameworkCfpCommonValidationAction
 		}
 	}
 
+        
+        
+    public void validateDateEqualOrGreater(Date cfpStartDate, Date cfpEndDate, String componentId) throws GtnFrameworkValidationFailedException {
+        if (cfpEndDate != null) {
+            if (cfpStartDate.equals(cfpEndDate)) {
+                throw new GtnFrameworkValidationFailedException(GtnFrameworkCfpStringContants.CFP_DATE_EQUAL_VALIDATION, componentId);
+            }
+            if (cfpStartDate.after(cfpEndDate)) {
+                throw new GtnFrameworkValidationFailedException(GtnFrameworkCfpStringContants.CFP_DATE_LESS_THAN_VALIDATION, componentId);
+            }
+        }
+    }
 	@Override
 	public GtnUIFrameWorkAction createInstance() {
 		return this;
