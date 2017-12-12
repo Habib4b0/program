4d0a3e0490ff6bf4eee73589d942d84944ce0be7
Deepika KrishnaKumar <deepika.krishnakumar@sysbiz.com>
2017-12-12 14:43:02 +0530

    ALG-2798 - Issue Fixed

--- app/gtnFrameworkMain/GtnFrameworkCommon/src/main/java/com/stpl/gtn/gtn2o/ws/constants/common/GtnFrameworkRegexStringConstants.java
+++ app/gtnFrameworkMain/GtnFrameworkCommon/src/main/java/com/stpl/gtn/gtn2o/ws/constants/common/GtnFrameworkRegexStringConstants.java
@@ -12,6 +12,10 @@ public class GtnFrameworkRegexStringConstants {
 	public static final String ALPHA_ERROR_MSG = " can only be Alphabets";
 
 	public static final String NUMERIC_DECIAL_DOUBLE_PRECISION_FORMAT = "^[0-9]+(\\.[0-9]{1,2})?$";
+	
+        public static final String NUMERIC_WITH_SIX_DECIMAL_PRECISION = "^[0-9]+(\\.[0-9]{1,6})?$";
+        
+        public static final String NUMERIC_WITH_THREE_DECIMAL_PRECISION = "^[0-9]+(\\.[0-9]{1,3})?$";
 
 	public static final String DATE_FORMAT = " ^[0-3][0-9]/[0-3][0-9]/(?:[0-9][0-9])?[0-9][0-9]$";
 
