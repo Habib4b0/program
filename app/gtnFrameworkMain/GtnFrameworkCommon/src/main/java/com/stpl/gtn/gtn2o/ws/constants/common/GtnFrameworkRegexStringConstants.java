9915c87723cd09b109115fd929b62373e9fd03c5
Deepika KrishnaKumar <deepika.krishnakumar@sysbiz.com>
2017-12-12 14:53:39 +0530

    Code Format

--- app/gtnFrameworkMain/GtnFrameworkCommon/src/main/java/com/stpl/gtn/gtn2o/ws/constants/common/GtnFrameworkRegexStringConstants.java
+++ app/gtnFrameworkMain/GtnFrameworkCommon/src/main/java/com/stpl/gtn/gtn2o/ws/constants/common/GtnFrameworkRegexStringConstants.java
@@ -12,10 +12,10 @@ public class GtnFrameworkRegexStringConstants {
 	public static final String ALPHA_ERROR_MSG = " can only be Alphabets";
 
 	public static final String NUMERIC_DECIAL_DOUBLE_PRECISION_FORMAT = "^[0-9]+(\\.[0-9]{1,2})?$";
-	
-        public static final String NUMERIC_WITH_SIX_DECIMAL_PRECISION = "^[0-9]+(\\.[0-9]{1,6})?$";
-        
-        public static final String NUMERIC_WITH_THREE_DECIMAL_PRECISION = "^[0-9]+(\\.[0-9]{1,3})?$";
+
+	public static final String NUMERIC_WITH_SIX_DECIMAL_PRECISION = "^[0-9]+(\\.[0-9]{1,6})?$";
+
+	public static final String NUMERIC_WITH_THREE_DECIMAL_PRECISION = "^[0-9]+(\\.[0-9]{1,3})?$";
 
 	public static final String DATE_FORMAT = " ^[0-3][0-9]/[0-3][0-9]/(?:[0-9][0-9])?[0-9][0-9]$";
 
@@ -36,7 +36,6 @@ public class GtnFrameworkRegexStringConstants {
 	public static final String ACCEPT_POSITIVE_AND_ZERO = "^[1-9]*|0$";
 
 	public static final String NUMERIC_WITH_DOT_REGEX = "^\\d+(\\.\\d+)*$";
-
 
 	private GtnFrameworkRegexStringConstants() {
 	}
