7173c4c832d619b60623a647e941628710a8842d
sivaramakrishnan.sri <trainees@sysbiz.com>
2017-12-18 15:35:45 +0530

    ALG-3104,ALG-3105,ALG-3127,ALG-2221

--- app/gtnFrameworkMain/Modules/GtnFrameworkUDCConfiguration/src/main/java/com/stpl/gtn/gtn2o/ui/module/udc/config/GtnFrameworkUDCConfigurationLandingScreenConfig.java
+++ app/gtnFrameworkMain/Modules/GtnFrameworkUDCConfiguration/src/main/java/com/stpl/gtn/gtn2o/ui/module/udc/config/GtnFrameworkUDCConfigurationLandingScreenConfig.java
@@ -98,13 +98,12 @@ public class GtnFrameworkUDCConfigurationLandingScreenConfig {
 		GtnUIFrameworkValidationConfig categoryValidationConfig = new GtnUIFrameworkValidationConfig();
 		categoryValidationConfig.setConditionList(Arrays.asList(GtnUIFrameworkConditionalValidationType.NOT_NULL));
 		udcCategory.setGtnUIFrameworkValidationConfig(categoryValidationConfig);
-
 		GtnUIFrameworkComboBoxConfig categoryConfig = componentConfig.getComboBoxConfig("CategoryName",
 				GtnWebServiceUrlConstants.GTN_COMMON_GENERAL_SERVICE
-						+ GtnWebServiceUrlConstants.GTN_COMMON_LOAD_COMBO_BOX);
-		categoryConfig.setItemValues(Arrays.asList("BRAND", "CategoryName","ALL"));
+				+ GtnWebServiceUrlConstants.GTN_COMMON_LOAD_COMBO_BOX);
+		
 		udcCategory.setGtnComboboxConfig(categoryConfig);
-
+       
 		List<GtnUIFrameWorkActionConfig> actionConfigList = new ArrayList<>();
 
 		GtnUIFrameWorkActionConfig customActionConfig = componentConfig
