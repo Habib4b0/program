160490378b6edb8ec22a0b691765f2bc35b0751a
lokeshwari.kumarasamy <lokeshwari.kumarasamy@sysbiz.com>
2017-12-21 20:49:20 +0530

    Resolved sonarfailure issue

--- app/gtnFrameworkMain/GtnFrameWorkPropertyManager/src/main/java/com/stpl/gtn/gtn2o/ws/GtnFileNameUtils.java
+++ app/gtnFrameworkMain/GtnFrameWorkPropertyManager/src/main/java/com/stpl/gtn/gtn2o/ws/GtnFileNameUtils.java
@@ -46,7 +46,7 @@ public class GtnFileNameUtils {
 		return new FileOutputStream(fileName);
 	}
 
-	public static FileWriter getFileWriter(Path path, String fileName) throws IOException {
+	public static FileWriter getFileWriter(String fileName) throws IOException {
 		return new FileWriter(fileName);
 	}
 }
