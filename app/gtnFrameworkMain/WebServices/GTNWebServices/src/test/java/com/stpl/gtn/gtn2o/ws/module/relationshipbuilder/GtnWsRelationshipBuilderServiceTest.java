fc47cceb1d4a911e041a30ae9acf04212c290327
Mohamed Shahul <mohamed.shahul@sysbiz.com>
2018-01-29 12:26:38 +0530

    GtnWsRelationshipBuilderServiceTest.java edited online with Bitbucket

--- app/gtnFrameworkMain/WebServices/GTNWebServices/src/test/java/com/stpl/gtn/gtn2o/ws/module/relationshipbuilder/GtnWsRelationshipBuilderServiceTest.java
+++ app/gtnFrameworkMain/WebServices/GTNWebServices/src/test/java/com/stpl/gtn/gtn2o/ws/module/relationshipbuilder/GtnWsRelationshipBuilderServiceTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
