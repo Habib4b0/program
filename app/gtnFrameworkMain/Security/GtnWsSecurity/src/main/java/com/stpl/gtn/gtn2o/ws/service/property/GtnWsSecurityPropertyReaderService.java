a70d669c3f822bce0f36d9d82f420c12c5c23e5d
Abishek ram <abishekram.r@sysbiz.com>
2018-02-26 18:37:12 +0530

    Added Try

--- app/gtnFrameworkMain/Security/GtnWsSecurity/src/main/java/com/stpl/gtn/gtn2o/ws/service/property/GtnWsSecurityPropertyReaderService.java
+++ app/gtnFrameworkMain/Security/GtnWsSecurity/src/main/java/com/stpl/gtn/gtn2o/ws/service/property/GtnWsSecurityPropertyReaderService.java
@@ -2,9 +2,12 @@ package com.stpl.gtn.gtn2o.ws.service.property;
 
 import com.stpl.gtn.gtn2o.ws.GtnFrameworkPropertyManager;
 import com.stpl.gtn.gtn2o.ws.bean.GtnWsSecurityPropertyBean;
+import com.stpl.gtn.gtn2o.ws.logger.GtnWSLogger;
+import com.stpl.gtn.gtn2o.ws.service.jwt.GtnWsSecuritySecretKey;
 
 public class GtnWsSecurityPropertyReaderService {
 	private static volatile GtnWsSecurityPropertyBean gtnWsSecurityPropertyBean;
+	private static final GtnWSLogger LOGGER = GtnWSLogger.getGTNLogger(GtnWsSecurityPropertyReaderService.class);
 
 	private GtnWsSecurityPropertyReaderService() {
 		super();
@@ -14,19 +17,24 @@ public class GtnWsSecurityPropertyReaderService {
 
 		if (gtnWsSecurityPropertyBean == null) {
 			gtnWsSecurityPropertyBean = new GtnWsSecurityPropertyBean();
-			gtnWsSecurityPropertyBean
-					.setJdbcdrriver(GtnFrameworkPropertyManager.getProperty("gtn.security.jdbc_driver"));
-			gtnWsSecurityPropertyBean.setDburl(GtnFrameworkPropertyManager.getProperty("gtn.security.db_url"));
-			gtnWsSecurityPropertyBean.setUsername(GtnFrameworkPropertyManager.getProperty("gtn.security.username"));
-			gtnWsSecurityPropertyBean.setPassword(GtnFrameworkPropertyManager.getProperty("gtn.security.password"));
-			gtnWsSecurityPropertyBean.setExpirationTime(
-					Long.parseLong(GtnFrameworkPropertyManager.getProperty("gtn.security.expirationTime.ms")));
-			gtnWsSecurityPropertyBean.setFilename(GtnFrameworkPropertyManager.getProperty("gtn.security.filename"));
-			gtnWsSecurityPropertyBean
-					.setDataSourceName(GtnFrameworkPropertyManager.getProperty("gtn.security.datasourceName"));
-			if ("true".equalsIgnoreCase(
-					GtnFrameworkPropertyManager.getProperty("gtn.security.connectionMode.datasource"))) {
-				gtnWsSecurityPropertyBean.setConnectDataSourceMode(true);
+			try {
+				gtnWsSecurityPropertyBean
+						.setJdbcdrriver(GtnFrameworkPropertyManager.getProperty("gtn.security.jdbc_driver"));
+				gtnWsSecurityPropertyBean.setDburl(GtnFrameworkPropertyManager.getProperty("gtn.security.db_url"));
+				gtnWsSecurityPropertyBean.setUsername(GtnFrameworkPropertyManager.getProperty("gtn.security.username"));
+				gtnWsSecurityPropertyBean.setPassword(GtnFrameworkPropertyManager.getProperty("gtn.security.password"));
+				gtnWsSecurityPropertyBean.setExpirationTime(
+						Long.parseLong(GtnFrameworkPropertyManager.getProperty("gtn.security.expirationTime.ms")));
+				gtnWsSecurityPropertyBean.setFilename(GtnFrameworkPropertyManager.getProperty("gtn.security.filename"));
+				gtnWsSecurityPropertyBean
+						.setDataSourceName(GtnFrameworkPropertyManager.getProperty("gtn.security.datasourceName"));
+				if ("true".equalsIgnoreCase(
+						GtnFrameworkPropertyManager.getProperty("gtn.security.connectionMode.datasource"))) {
+					gtnWsSecurityPropertyBean.setConnectDataSourceMode(true);
+				}
+			} catch (Exception e) {
+				LOGGER.error("Exception in initialaising security property bean " + e);
+
 			}
 		}
 		return gtnWsSecurityPropertyBean;
