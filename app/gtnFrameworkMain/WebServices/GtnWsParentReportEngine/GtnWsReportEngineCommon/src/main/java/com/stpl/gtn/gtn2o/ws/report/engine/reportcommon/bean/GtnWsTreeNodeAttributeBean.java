33f945ae1755386bec99995f8e32c429b15fc953
Jayaram L <jayaram.l@sysbiz.com>
2018-04-19 12:52:15 +0530

    done logic for inserting tree to mongodb and retrieve back

--- app/gtnFrameworkMain/WebServices/GtnWsParentReportEngine/GtnWsReportEngineCommon/src/main/java/com/stpl/gtn/gtn2o/ws/report/engine/reportcommon/bean/GtnWsTreeNodeAttributeBean.java
+++ app/gtnFrameworkMain/WebServices/GtnWsParentReportEngine/GtnWsReportEngineCommon/src/main/java/com/stpl/gtn/gtn2o/ws/report/engine/reportcommon/bean/GtnWsTreeNodeAttributeBean.java
@@ -1,11 +1,20 @@
 package com.stpl.gtn.gtn2o.ws.report.engine.reportcommon.bean;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GtnWsTreeNodeAttributeBean {
+public class GtnWsTreeNodeAttributeBean implements Serializable {
 
-	List<GtnWsAttributeBean> attributeBeanList = null;
+	private static final long serialVersionUID = 1L;
+
+	public GtnWsTreeNodeAttributeBean() {
+		super();
+	}
+
+	List<GtnWsAttributeBean> attributeBeanList;
 
 	public List<GtnWsAttributeBean> getAttributeBeanList() {
 		return attributeBeanList;
@@ -18,4 +27,12 @@ public class GtnWsTreeNodeAttributeBean {
 		this.attributeBeanList.add(attributeBean);
 	}
 
+	private void writeObject(java.io.ObjectOutputStream s) throws java.io.IOException {
+		s.defaultWriteObject();
+	}
+
+	// Dont delete. this Method is called during Serialization
+	private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
+		ois.defaultReadObject();
+	}
 }
