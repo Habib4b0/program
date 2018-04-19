bb3c124b8c5af4ab734dd717b1c6ee6ab777ab92
SYSBIZ\vinodhini.palanisamy <vinodhini.palanisamy@sysbiz.com>
2018-04-19 18:15:23 +0530

    commit Report webservice changes

--- app/gtnFrameworkMain/WebServices/GtnWsParentReportEngine/GtnReportingWebservice/src/main/java/com/stpl/gtn/gtn2o/ws/controller/GtnWsReportController.java
+++ app/gtnFrameworkMain/WebServices/GtnWsParentReportEngine/GtnReportingWebservice/src/main/java/com/stpl/gtn/gtn2o/ws/controller/GtnWsReportController.java
@@ -1,7 +1,6 @@
 package com.stpl.gtn.gtn2o.ws.controller;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,62 +9,98 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stpl.gtn.gtn2o.queryengine.engine.GtnFrameworkSqlQueryEngine;
+import com.stpl.gtn.gtn2o.ws.components.GtnUIFrameworkDataTable;
 import com.stpl.gtn.gtn2o.ws.exception.GtnFrameworkGeneralException;
-import com.stpl.gtn.gtn2o.ws.logger.GtnQueryLogger;
 import com.stpl.gtn.gtn2o.ws.logger.GtnWSLogger;
+import com.stpl.gtn.gtn2o.ws.report.bean.CustomerHierarchyLookupBean;
+import com.stpl.gtn.gtn2o.ws.report.constants.GtnWsReportConstants;
 import com.stpl.gtn.gtn2o.ws.request.GtnUIFrameworkWebserviceRequest;
-import com.stpl.gtn.gtn2o.ws.request.GtnWsGeneralRequest;
-import com.stpl.gtn.gtn2o.ws.response.GtnUIFrameworkWebserviceComboBoxResponse;
+import com.stpl.gtn.gtn2o.ws.request.report.GtnWsReportRequest;
+import com.stpl.gtn.gtn2o.ws.response.GtnSerachResponse;
 import com.stpl.gtn.gtn2o.ws.response.GtnUIFrameworkWebserviceResponse;
-import com.stpl.gtn.gtn2o.ws.response.GtnWsGeneralResponse;
+import com.stpl.gtn.gtn2o.ws.service.GtnWsReportWebsevice;
 
 @RestController
-public class GtnWsGeneralController {
+@RequestMapping(value = GtnWsReportConstants.GTN_REPORT_SERVICE)
+public class GtnWsReportController {
 
-	GtnWSLogger gtnLogger = GtnWSLogger.getGTNLogger(GtnWsGeneralController.class);
-	@RequestMapping(value = "/gtnWsReportComboboxLoad", method = RequestMethod.POST)
-	public GtnUIFrameworkWebserviceResponse getComboBoxResultSet(
-			@RequestBody GtnUIFrameworkWebserviceRequest gtnWsRequest) {
-		System.out.println("----inside-GtnWsGeneralController-----------");
-		GtnUIFrameworkWebserviceResponse gtnResponse = new GtnUIFrameworkWebserviceResponse();
-		GtnWsGeneralResponse generalWSResponse = new GtnWsGeneralResponse();
-		generalWSResponse.setSucess(true);
+	public GtnWsReportController() {
 
-		try {
-			GtnWsReportAllListConfig gtnWsReportAllListConfig=new GtnWsReportAllListConfig();
-			Map<String, String> comboBoxTypeResponseMap = gtnWsReportAllListConfig.getComboboxLoadMap();
-			GtnWsGeneralRequest generalWSRequest = gtnWsRequest.getGtnWsGeneralRequest();
-			String comboBoxType = generalWSRequest.getComboBoxType();
-			String query = comboBoxTypeResponseMap.get(comboBoxType);
+	}
 
-			List<Object[]> resultList = null;
-			if (query != null) {
-				comboBoxType = getComboboxTypeForReportFromAndToDate(comboBoxType);
-				gtnLogger.info("-----comboBoxType-----------"+comboBoxType);
-				resultList=executeQuery(comboBoxType);
-				gtnLogger.info("-----resultList-----------"+resultList.get(0));
-				GtnUIFrameworkWebserviceComboBoxResponse comboBoxResponse = new GtnUIFrameworkWebserviceComboBoxResponse();
-				comboBoxResponse.setComboBoxList(resultList);
-				gtnResponse.setGtnUIFrameworkWebserviceComboBoxResponse(comboBoxResponse);
-			}
+	GtnWSLogger gtnLogger = GtnWSLogger.getGTNLogger(GtnWsReportController.class);
 
-		} catch (Exception exception) {
-			gtnLogger.error("Exception in "+exception);
+	@Autowired
+	private GtnWsReportWebsevice gtnWsReportWebsevice;
+	@Autowired
+	private GtnFrameworkSqlQueryEngine gtnSqlQueryEngine;
+
+	@Autowired
+	private org.hibernate.SessionFactory sessionFactory;
+
+	public org.hibernate.SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(org.hibernate.SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	public GtnFrameworkSqlQueryEngine getGtnSqlQueryEngine() {
+		return gtnSqlQueryEngine;
+	}
+
+	public void setGtnSqlQueryEngine(GtnFrameworkSqlQueryEngine gtnSqlQueryEngine) {
+		this.gtnSqlQueryEngine = gtnSqlQueryEngine;
+	}
+
+	@RequestMapping(value = GtnWsReportConstants.GTN_REPORT_CUSTOMERHIERARCHY_SEARCHSERVICE, method = RequestMethod.POST)
+	public GtnUIFrameworkWebserviceResponse loadHierarchyResults(@RequestBody GtnUIFrameworkWebserviceRequest request)
+			throws GtnFrameworkGeneralException {
+		List<Object[]> resultList;
+		GtnWsReportRequest gtnWsReportRequest = request.getGtnWsReportRequest();
+		GtnSerachResponse gtnSearchResponse = new GtnSerachResponse();
+
+                GtnUIFrameworkWebserviceResponse response = new GtnUIFrameworkWebserviceResponse();
+		if (gtnWsReportRequest != null) {
+			CustomerHierarchyLookupBean hierarchyBean = gtnWsReportRequest.getCustomerHierarchyLookupBean();
+			resultList = gtnWsReportWebsevice.loadHierarchyResults(hierarchyBean);
+		}else{
+			resultList = gtnWsReportWebsevice.loadHierarchyResults();
 		}
-		gtnResponse.setGtnWsGeneralResponse(generalWSResponse);
-		return gtnResponse;
+		GtnUIFrameworkDataTable dataTable = new GtnUIFrameworkDataTable();
+		dataTable.addData(resultList);
+		gtnSearchResponse.setResultSet(dataTable);
+		response.setGtnSerachResponse(gtnSearchResponse);
+		return response;
 	}
 
-	private String getComboboxTypeForReportFromAndToDate(String comboBoxType) {
-		String mainQuery = ";WITH cte AS "
-				+ "(SELECT TOP 1 FROM_DATE AS FROM_DATE, TO_DATE AS  TO_DATE FROM   forecast_config f JOIN HELPER_TABLE h ON f.BUSINESS_PROCESS_TYPE = h.HELPER_TABLE_SID WHERE  LIST_NAME LIKE 'BUSINESS_PROCESS_TYPE' AND DESCRIPTION = 'Commercial' ORDER BY VERSION_NO DESC) SELECT    DESCRIPTION  FROM   forecast_config f JOIN HELPER_TABLE h ON f.HIST_FREQ = h.HELPER_TABLE_SID WHERE  FROM_DATE=(SELECT FROM_DATE FROM   cte c)";
-
-		return mainQuery;
+	@RequestMapping(value = GtnWsReportConstants.GTN_REPORT_PRODUCTHIERARCHY_SEARCHSERVICE, method = RequestMethod.POST)
+	public GtnUIFrameworkWebserviceResponse loadProductHierarchyResults(
+			@RequestBody GtnUIFrameworkWebserviceRequest request) throws GtnFrameworkGeneralException {
+		List<Object[]> resultList;
+		GtnWsReportRequest gtnWsReportRequest = request.getGtnWsReportRequest();
+		GtnSerachResponse gtnSearchResponse = new GtnSerachResponse();
+		GtnUIFrameworkWebserviceResponse response = new GtnUIFrameworkWebserviceResponse();
+		if (gtnWsReportRequest != null) {
+			CustomerHierarchyLookupBean hierarchyBean = gtnWsReportRequest.getCustomerHierarchyLookupBean();
+			resultList = gtnWsReportWebsevice.loadProductHierarchyResults(hierarchyBean);
+		}else{
+			resultList = gtnWsReportWebsevice.loadProductHierarchyResults();
+		}
+		GtnUIFrameworkDataTable dataTable = new GtnUIFrameworkDataTable();
+		dataTable.addData(resultList);
+		gtnSearchResponse.setResultSet(dataTable);
+		response.setGtnSerachResponse(gtnSearchResponse);
+		return response;
 	}
 
-	@SuppressWarnings({ "rawtypes" })
-	public List executeQuery(String sqlQuery) throws GtnFrameworkGeneralException {
-		GtnFrameworkSqlQueryEngine gtnSqlQueryEngine=new GtnFrameworkSqlQueryEngine();
-		return gtnSqlQueryEngine.executeSelectQuery(sqlQuery);
+	@RequestMapping(value = GtnWsReportConstants.GTN_REPORT_LOADRELATIONSHIP_SERVICE, method = RequestMethod.POST)
+	public GtnUIFrameworkWebserviceResponse loadRelationshipValues(@RequestBody GtnUIFrameworkWebserviceRequest request) throws GtnFrameworkGeneralException{
+		GtnWsReportRequest gtnWsReportRequest = request.getGtnWsReportRequest();
+		CustomerHierarchyLookupBean lookupBean = gtnWsReportRequest.getCustomerHierarchyLookupBean();
+		GtnUIFrameworkWebserviceResponse response = new GtnUIFrameworkWebserviceResponse();
+		gtnWsReportWebsevice.loadRelationshipValues(lookupBean);
+		return response;
 	}
 }
