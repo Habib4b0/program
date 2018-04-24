873ed5d84d84d0befebb2897309a21bea393fcf3
unknown <Hazihabibullah.Syed@STPLDEV66.sysbiz.org>
2018-04-24 18:53:22 +0530

    Fixed Sonar Issue

--- app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/nationalassumptions/queryutils/FcpQueryUtils.java
+++ app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/nationalassumptions/queryutils/FcpQueryUtils.java
@@ -169,7 +169,7 @@ public class FcpQueryUtils {
                             } else {
                                 queryBuilder1.append(" UPDATE dbo.ST_FCP_PROJ SET ADJUSTMENT= ").append(Constant.NULL_CAPS);
                             }
-                        } else if (pricetype.equals(Constant.QFSS) || pricetype.equals(Constant.QNONFAMP)) {
+                        } else if (Constant.QFSS.equals(pricetype) || Constant.QNONFAMP.equals(pricetype)) {
                             queryBuilder1.append(" UPDATE dbo.ST_FCP_PROJ SET ADJUSTMENT='").append(finalvalue).append("' ")
                                     .append(" , ADJUSTMENT_PRICE='").append(finalvalue).append("' ");
                         } else {
