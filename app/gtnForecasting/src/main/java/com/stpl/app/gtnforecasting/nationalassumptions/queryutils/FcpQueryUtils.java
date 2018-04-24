9deef6b9843cebce8e0c3e33f53bc5d7584d08c5
unknown <Hazihabibullah.Syed@STPLDEV66.sysbiz.org>
2018-04-24 15:32:11 +0530

    ALG-4430,ALG-4384

--- app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/nationalassumptions/queryutils/FcpQueryUtils.java
+++ app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/nationalassumptions/queryutils/FcpQueryUtils.java
@@ -169,7 +169,7 @@ public class FcpQueryUtils {
                             } else {
                                 queryBuilder1.append(" UPDATE dbo.ST_FCP_PROJ SET ADJUSTMENT= ").append(Constant.NULL_CAPS);
                             }
-                        } else if (pricetype.equals("QFSS")) {
+                        } else if (pricetype.equals("QFSS") || pricetype.equals("QNON-FAMP")) {
                             queryBuilder1.append(" UPDATE dbo.ST_FCP_PROJ SET ADJUSTMENT='").append(finalvalue).append("' ")
                                     .append(" , ADJUSTMENT_PRICE='").append(finalvalue).append("' ");
                         } else {
