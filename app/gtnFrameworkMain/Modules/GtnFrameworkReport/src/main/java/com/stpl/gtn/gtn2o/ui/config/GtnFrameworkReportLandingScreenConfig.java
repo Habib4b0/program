a5f9af2ee91fe58112df410008bb2e7eae2255ed
SYSBIZ\vinodhini.palanisamy <vinodhini.palanisamy@sysbiz.com>
2018-05-09 14:26:58 +0530

    commit Reporting changes

--- app/gtnFrameworkMain/Modules/GtnFrameworkReport/src/main/java/com/stpl/gtn/gtn2o/ui/config/GtnFrameworkReportLandingScreenConfig.java
+++ app/gtnFrameworkMain/Modules/GtnFrameworkReport/src/main/java/com/stpl/gtn/gtn2o/ui/config/GtnFrameworkReportLandingScreenConfig.java
@@ -345,7 +345,7 @@ public class GtnFrameworkReportLandingScreenConfig {
 		fromPeriod.setGtnComboboxConfig(fromPeriodConfig);
 
 	
-//		componentList.add(fromPeriod);
+		componentList.add(fromPeriod);
 
 
 		GtnUIFrameworkLayoutConfig toPeriodLayout = new GtnUIFrameworkLayoutConfig();
@@ -384,7 +384,7 @@ public class GtnFrameworkReportLandingScreenConfig {
 
 		toPeriod.setGtnComboboxConfig(toPeriodTypeConfig);
 
-//		componentList.add(toPeriod);
+		componentList.add(toPeriod);
 
 	}
 
