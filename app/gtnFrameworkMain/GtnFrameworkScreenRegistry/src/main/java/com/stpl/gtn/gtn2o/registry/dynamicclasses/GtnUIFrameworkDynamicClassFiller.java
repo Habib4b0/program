013ec391c281182b81c47a1cf22de469f7a64bc0
Spandan Majumder <trainees@sysbiz.com>
2018-05-22 18:06:29 +0530

    commmit changes dynamic class filler of screen registry regarding conflicts issue

--- app/gtnFrameworkMain/GtnFrameworkScreenRegistry/src/main/java/com/stpl/gtn/gtn2o/registry/dynamicclasses/GtnUIFrameworkDynamicClassFiller.java
+++ app/gtnFrameworkMain/GtnFrameworkScreenRegistry/src/main/java/com/stpl/gtn/gtn2o/registry/dynamicclasses/GtnUIFrameworkDynamicClassFiller.java
@@ -8,26 +8,8 @@ import com.stpl.gtn.gtn2o.registry.action.pagedtreetable.GtnFrameworkFSPagedTree
 import com.stpl.gtn.gtn2o.registry.action.pagedtreetable.GtnFrameworkFSPagedTreeTableGetBulkDataAction;
 import com.stpl.gtn.gtn2o.registry.action.pagedtreetable.GtnFrameworkFSPagedTreeTableGetCountAction;
 import com.stpl.gtn.gtn2o.registry.action.pagedtreetable.GtnFrameworkReturnCheckAllAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnCustomerAvailableTableLoadAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkCustomTreeConfirmedSaveAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUIBuildCustomTreeAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUICustomTreeAddAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUICustomTreeRemoveAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUICustomTreeSaveAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUICustomVariableGridLoadAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUICustomVariablePositionChangeAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUICustomViewEditAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUICustomViewHierarchyLoadAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkUIReportDasboardTableLoadAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnProductLevelAvailableTableLoadAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnReportCCPTableLoadAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnReportDataSelectionResetAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnReportDataSelectionTabLoadAction;
-import com.stpl.gtn.gtn2o.ui.action.GtnUIFrameworkSaveViewAction;
 import com.stpl.gtn.gtn2o.ui.framework.engine.GtnUIFrameworkGlobalUI;
 import com.stpl.gtn.gtn2o.ui.framework.engine.base.GtnUIDynamicObjectFiller;
-import com.stpl.gtn.gtn2o.ui.module.lookups.action.GtnForecastEligibleDateLoadAction;
-import com.stpl.gtn.gtn2o.ui.module.lookups.action.GtnRelationshipVersionLoadAction;
 
 public class GtnUIFrameworkDynamicClassFiller implements GtnUIDynamicObjectFiller {
 
@@ -55,46 +37,7 @@ public class GtnUIFrameworkDynamicClassFiller implements GtnUIDynamicObjectFille
 		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkReturnCheckAllAction.class.getName(),
 				new GtnFrameworkReturnCheckAllAction());
 
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnRelationshipVersionLoadAction.class.getName(),
-				new GtnRelationshipVersionLoadAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnCustomerAvailableTableLoadAction.class.getName(),
-				new GtnCustomerAvailableTableLoadAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnProductLevelAvailableTableLoadAction.class.getName(),
-				new GtnProductLevelAvailableTableLoadAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUICustomViewHierarchyLoadAction.class.getName(),
-				new GtnFrameworkUICustomViewHierarchyLoadAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnReportCCPTableLoadAction.class.getName(),
-				new GtnReportCCPTableLoadAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUICustomTreeAddAction.class.getName(),
-				new GtnFrameworkUICustomTreeAddAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUICustomTreeSaveAction.class.getName(),
-				new GtnFrameworkUICustomTreeSaveAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUICustomVariablePositionChangeAction.class.getName(),
-				new GtnFrameworkUICustomVariablePositionChangeAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUICustomVariableGridLoadAction.class.getName(),
-				new GtnFrameworkUICustomVariableGridLoadAction());
-
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnReportDataSelectionTabLoadAction.class.getName(),
-				new GtnReportDataSelectionTabLoadAction());
-
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUICustomTreeRemoveAction.class.getName(),
-				new GtnFrameworkUICustomTreeRemoveAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUIBuildCustomTreeAction.class.getName(),
-				new GtnFrameworkUIBuildCustomTreeAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUIReportDasboardTableLoadAction.class.getName(),
-				new GtnFrameworkUIReportDasboardTableLoadAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnUIFrameworkSaveViewAction.class.getName(),
-				new GtnUIFrameworkSaveViewAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnReportDataSelectionResetAction.class.getName(),
-				new GtnReportDataSelectionResetAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnForecastEligibleDateLoadAction.class.getName(),
-				new GtnForecastEligibleDateLoadAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkCustomTreeConfirmedSaveAction.class.getName(),
-				new GtnFrameworkCustomTreeConfirmedSaveAction());
-		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkUICustomViewEditAction.class.getName(),
-				new GtnFrameworkUICustomViewEditAction());
-
-
+		
 	}
 
 }
