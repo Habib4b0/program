53df8b5801da325ad409d1e9664f9da7aba59690
Gokulkumar Murugesan <gokulkumar.murugesan@sysbiz.com>
2018-06-19 20:39:48 +0530

    commit reporting sonar fix

--- app/gtnFrameworkMain/GtnFrameworkCommon/src/main/java/com/stpl/gtn/gtn2o/ws/report/bean/GtnReportVariableBreakdownLookupBean.java
+++ app/gtnFrameworkMain/GtnFrameworkCommon/src/main/java/com/stpl/gtn/gtn2o/ws/report/bean/GtnReportVariableBreakdownLookupBean.java
@@ -12,50 +12,135 @@ import java.util.List;
  * @author gokulkumar.murugesan
  */
 public class GtnReportVariableBreakdownLookupBean {
-    
-    private int masterSid;
-    private int period;
-    private int year;
-    private int selectedVariable;
-    private List<Object[]> resultList;
 
-    public List<Object[]> getResultList() {
-        return resultList;
-    }
+	private int masterSid;
+	private int period;
+	private int year;
+	private int selectedVariable;
+	private List<Object[]> resultList;
 
-    public void setResultList(List<Object[]> resultList) {
-        this.resultList = resultList;
-    }
-    public int getMasterSid() {
-        return masterSid;
-    }
+	private String property;
+	private String componentId;
+	private int rowId;
+	private int col;
+	private String currentDateField;
 
-    public void setMasterSid(int masterSid) {
-        this.masterSid = masterSid;
-    }
+	private String projectionName;
 
-    public int getPeriod() {
-        return period;
-    }
+	private List variableBreakdownSaveActionList;
+	private int rowCount;
+	private List<GtnReportComparisonProjectionBean> comparisonLookupBeanList;
 
-    public void setPeriod(int period) {
-        this.period = period;
-    }
+	public List<Object[]> getResultList() {
+		return resultList;
+	}
 
-    public int getYear() {
-        return year;
-    }
+	public void setResultList(List<Object[]> resultList) {
+		this.resultList = resultList;
+	}
 
-    public void setYear(int year) {
-        this.year = year;
-    }
+	public int getMasterSid() {
+		return masterSid;
+	}
 
-    public int getSelectedVariable() {
-        return selectedVariable;
-    }
+	public void setMasterSid(int masterSid) {
+		this.masterSid = masterSid;
+	}
 
-    public void setSelectedVariable(int selectedVariable) {
-        this.selectedVariable = selectedVariable;
-    }
-   
+	public int getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(int period) {
+		this.period = period;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getSelectedVariable() {
+		return selectedVariable;
+	}
+
+	public void setSelectedVariable(int selectedVariable) {
+		this.selectedVariable = selectedVariable;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	public String getComponentId() {
+		return componentId;
+	}
+
+	public void setComponentId(String componentId) {
+		this.componentId = componentId;
+	}
+
+	public int getRowId() {
+		return rowId;
+	}
+
+	public void setRowId(int rowId) {
+		this.rowId = rowId;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public String getCurrentDateField() {
+		return currentDateField;
+	}
+
+	public void setCurrentDateField(String currentDateField) {
+		this.currentDateField = currentDateField;
+	}
+
+	public String getProjectionName() {
+		return projectionName;
+	}
+
+	public void setProjectionName(String projectionName) {
+		this.projectionName = projectionName;
+	}
+
+	public List getVariableBreakdownSaveActionList() {
+		return variableBreakdownSaveActionList;
+	}
+
+	public void setVariableBreakdownSaveActionList(List variableBreakdownSaveActionList) {
+		this.variableBreakdownSaveActionList = variableBreakdownSaveActionList;
+	}
+
+	public int getRowCount() {
+		return rowCount;
+	}
+
+	public void setRowCount(int rowCount) {
+		this.rowCount = rowCount;
+	}
+
+	public List<GtnReportComparisonProjectionBean> getComparisonLookupBeanList() {
+		return comparisonLookupBeanList;
+	}
+
+	public void setComparisonLookupBeanList(List<GtnReportComparisonProjectionBean> comparisonLookupBeanList) {
+		this.comparisonLookupBeanList = comparisonLookupBeanList;
+	}
+
 }
