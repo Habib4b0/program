1394da12b2cdeaf9a55ef47ac9d66d618579d56f
SYSBIZ\mekalai.madhappa <mekalai.madhappa@sysbiz.com>
2018-06-19 18:28:19 +0530

    Removed unused import

--- app/gtnFrameworkMain/Modules/GtnFrameworkRelationshipBuilder/src/main/java/com/stpl/gtn/gtn2o/ui/module/relationshipbuilder/config/action/GtnFrameworkAutoBuildAction.java
+++ app/gtnFrameworkMain/Modules/GtnFrameworkRelationshipBuilder/src/main/java/com/stpl/gtn/gtn2o/ui/module/relationshipbuilder/config/action/GtnFrameworkAutoBuildAction.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameWorkAction;
 import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameWorkActionConfig;
-import com.stpl.gtn.gtn2o.ui.framework.action.executor.GtnUIFrameworkActionExecutor;
 import com.stpl.gtn.gtn2o.ui.framework.component.listener.GtnUIMsgBoxTwoButtonListener;
 import com.stpl.gtn.gtn2o.ui.framework.engine.GtnUIFrameworkGlobalUI;
 import com.stpl.gtn.gtn2o.ui.framework.engine.base.GtnUIFrameworkBaseComponent;
