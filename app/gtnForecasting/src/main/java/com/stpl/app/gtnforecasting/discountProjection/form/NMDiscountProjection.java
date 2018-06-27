cd3e40d3efeb1cc8935b54e48c31b3203349a2d4
manikanda prabu <manikanda.prabu@sysbiz.com>
2018-06-27 17:38:42 +0530

    Single period methodology custom view issue fix

--- app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/discountProjection/form/NMDiscountProjection.java
+++ app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/discountProjection/form/NMDiscountProjection.java
@@ -2499,7 +2499,7 @@ public class NMDiscountProjection extends ForecastDiscountProjection {
     @Override
     protected void customCalculateBtnClickLogic() {
         try {
-
+             session.setFunctionMode("CALC");
             if (CONTRACT_DETAILS.getConstant().equals(methodologyDdlb.getValue())) {
                 CommonUtil.getInstance().waitsForOtherThreadsToComplete(
                         session.getFutureValue(Constant.CALL_PRC_CONTRACT_DETAILS_REBATE));
@@ -2627,7 +2627,11 @@ public class NMDiscountProjection extends ForecastDiscountProjection {
                                                             "st_disc_growth_factor_", session.getSelectedRsForCustom(),
                                                             String.valueOf(level.getValue()));
                                                 }
+                                                
                                                 discountProjectionLogic.callDPProcedure(session, projectionSelection);
+                                                new DataSelectionLogic().callViewInsertProceduresThread(session, Constant.DISCOUNT3,"","","");
+                                                CommonUtil.getInstance().waitForSeconds();
+                                                CommonLogic.procedureCompletionCheck(session, DISCOUNT, com.stpl.app.serviceUtils.Constants.CUSTOM);
                                                 refreshTableData(getCheckedRecordsHierarchyNo());
                                                 final Notification notif = new Notification(
                                                         Constant.CALCULATION_COMPLETE,
