f6f66341a3a18bc3210295a1dca001c8eae24d63
Vishal Chaudhary <vishal.chaudhary@sysbiz.com>
2018-07-04 20:40:50 +0530

    GAL-13531 IM

---
+++ app/gtnFrameworkMain/Modules/GtnFrameworkItemMaster/src/main/java/com/stpl/gtn/gtn2o/ui/module/itemmaster/action/GtnFrameworkItemMasterAddViewCreationAction.java
@@ -0,0 +1,114 @@
+package com.stpl.gtn.gtn2o.ui.module.itemmaster.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameWorkAction;
+import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameWorkActionConfig;
+import com.stpl.gtn.gtn2o.ui.framework.action.GtnUIFrameworkActionShareable;
+import com.stpl.gtn.gtn2o.ui.framework.engine.GtnUIFrameworkGlobalUI;
+import com.stpl.gtn.gtn2o.ui.framework.engine.base.GtnUIFrameworkDynamicClass;
+import com.stpl.gtn.gtn2o.ui.framework.type.GtnUIFrameworkModeType;
+import com.stpl.gtn.gtn2o.ui.module.itemmaster.constants.GtnFrameworkItemMasterStringContants;
+import com.stpl.gtn.gtn2o.ui.module.itemmaster.util.GtnFrameworkItemMasterArmUdc1Utility;
+import com.stpl.gtn.gtn2o.ws.exception.GtnFrameworkGeneralException;
+import com.stpl.gtn.gtn2o.ws.exception.GtnFrameworkValidationFailedException;
+import com.stpl.gtn.gtn2o.ws.logger.GtnWSLogger;
+
+public class GtnFrameworkItemMasterAddViewCreationAction implements GtnUIFrameWorkAction, GtnUIFrameworkActionShareable, GtnUIFrameworkDynamicClass  {
+	private GtnWSLogger gtnLogger = GtnWSLogger.getGTNLogger(GtnFrameworkItemMasterAddViewCreationAction.class);
+
+	@Override
+	public void configureParams(GtnUIFrameWorkActionConfig gtnUIFrameWorkActionConfig)
+			throws GtnFrameworkGeneralException {
+		/*
+		 * No Implementation Needed
+		 */
+	}
+
+	@Override
+	public void doAction(String componentId, GtnUIFrameWorkActionConfig gtnUIFrameWorkActionConfig)
+			throws GtnFrameworkGeneralException {
+		gtnLogger.debug("Inside GtnFrameworkItemMasterAddViewCreationAction --> doAction... start");
+		checkUDC1Type();
+		String udc1Type=(String) GtnUIFrameworkGlobalUI.getSessionProperty("UDC1");
+		
+		/*if(GtnUIFrameworkGlobalUI.getSessionProperty("mode").equals(GtnUIFrameworkModeType.VIEW)) {
+			activeUdc1TextBox();
+			return;
+		}*/
+		
+		if(udc1Type!=null && udc1Type.equalsIgnoreCase(GtnFrameworkItemMasterStringContants.ARM_UDC_1)) {
+			if(GtnUIFrameworkGlobalUI.getSessionProperty("mode").equals(GtnUIFrameworkModeType.VIEW)) {
+				activeUdc1TextBox();
+				return;
+			}
+			activeCheckedComboBoxUdc1();
+			return;
+		}
+		activeComboBoxUdc1();
+		
+		gtnLogger.debug("Inside GtnFrameworkItemMasterAddViewCreationAction --> doAction... end");
+	}
+
+	@Override
+	public GtnUIFrameWorkAction createInstance() {
+		return this;
+	}
+	
+	private void activeUdc1TextBox() {
+		GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_TEXT_BOX_LAYOUT)
+			.setVisible(Boolean.TRUE);
+		GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_LAYOUT)
+			.setVisible(Boolean.FALSE);
+		GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_CHECKED_COMBO_BOX_LAYOUT)
+			.setVisible(Boolean.FALSE);
+	}
+	
+	private void checkUDC1Type() {
+		String udc1Type = System.getProperty(GtnFrameworkItemMasterStringContants.UDC1);
+		GtnUIFrameworkGlobalUI.addSessionProperty(GtnFrameworkItemMasterStringContants.UDC1, udc1Type);
+	}
+	
+	private void activeComboBoxUdc1() {
+		/*
+		 * Visibility Of Udc1 Component
+		 */
+		
+		GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_LAYOUT)
+			.setVisible(Boolean.TRUE);
+		GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_CHECKED_COMBO_BOX_LAYOUT)
+			.setVisible(Boolean.FALSE);
+		GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_TEXT_BOX_LAYOUT)
+			.setVisible(Boolean.FALSE);
+	}
+
+	private void activeCheckedComboBoxUdc1() {
+		try {
+			List<String> itemValueList=new ArrayList<>(GtnFrameworkItemMasterArmUdc1Utility.getArmUdc1ValueList());
+			List<String> itemCodeList=new ArrayList<>(GtnFrameworkItemMasterArmUdc1Utility.getArmUdc1CodeList());
+			
+			for(int i=0;i<itemValueList.size();i++) {
+				if (itemValueList.get(i).contains(GtnFrameworkItemMasterStringContants.COMMA)) {
+					itemValueList.remove(i);
+					itemCodeList.remove(i);
+				}
+			}
+			
+			GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_CHECKED_COMBO_BOX)
+				.loadCheckedCombobox(GtnFrameworkItemMasterStringContants.SELECT_VALUE, itemCodeList, itemValueList);
+			/*
+			 * Visibility Of Udc1 Component
+			 */
+			GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_CHECKED_COMBO_BOX_LAYOUT)
+				.setVisible(Boolean.TRUE);
+			GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_LAYOUT)
+				.setVisible(Boolean.FALSE);
+			GtnUIFrameworkGlobalUI.getVaadinBaseComponent(GtnFrameworkItemMasterStringContants.ITEM_INFORMATION_TAB_UDC_1_TEXT_BOX_LAYOUT)
+				.setVisible(Boolean.FALSE);
+		} catch (GtnFrameworkValidationFailedException e) {
+			gtnLogger.error("Error in GtnFrameworkItemMasterAddViewCreationAction --> activeCheckedComboBoxUdc1", e);
+		}
+	}
+
+}
---
+++ app/gtnFrameworkMain/Modules/GtnFrameworkItemMaster/src/main/java/com/stpl/gtn/gtn2o/ui/module/itemmaster/util/GtnFrameworkItemMasterArmUdc1Utility.java
@@ -0,0 +1,117 @@
+package com.stpl.gtn.gtn2o.ui.module.itemmaster.util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.stpl.gtn.gtn2o.ui.framework.engine.GtnUIFrameworkGlobalUI;
+import com.stpl.gtn.gtn2o.ui.module.itemmaster.constants.GtnFrameworkItemMasterStringContants;
+import com.stpl.gtn.gtn2o.ws.GtnUIFrameworkWebServiceClient;
+import com.stpl.gtn.gtn2o.ws.constants.url.GtnWebServiceUrlConstants;
+import com.stpl.gtn.gtn2o.ws.logger.GtnWSLogger;
+import com.stpl.gtn.gtn2o.ws.request.GtnUIFrameworkWebserviceRequest;
+import com.stpl.gtn.gtn2o.ws.request.GtnWsGeneralRequest;
+import com.stpl.gtn.gtn2o.ws.response.GtnUIFrameworkWebserviceComboBoxResponse;
+
+public class GtnFrameworkItemMasterArmUdc1Utility {
+	
+	private static GtnWSLogger gtnLogger = GtnWSLogger.getGTNLogger(GtnFrameworkItemMasterArmUdc1Utility.class);
+	private static List<String> armUdc1ValueList=null;
+	private static List<String> armUdc1CodeList=null;
+	private static Map<Integer, String> codeValueArmUdc1HashMap = null;
+	
+	private GtnFrameworkItemMasterArmUdc1Utility() {
+		/*
+		 * Singleton Class
+		 */
+	}
+	
+	private static void createValueCodeHashMap() {
+		if(codeValueArmUdc1HashMap != null) {
+			return;
+		}
+		codeValueArmUdc1HashMap= new HashMap<>();
+		
+        for(int i=0;i<armUdc1ValueList.size();i++)
+        {
+            codeValueArmUdc1HashMap.put(Integer.parseInt(armUdc1CodeList.get(i)),armUdc1ValueList.get(i));
+        }
+	}
+	
+	private static void getValuesForArmUdc1FromHelperTable() {
+		if(armUdc1ValueList != null) {
+			return;
+		}
+		
+		GtnWsGeneralRequest generalWSRequest = new GtnWsGeneralRequest();
+		generalWSRequest.setComboBoxType(GtnFrameworkItemMasterStringContants.ARM_UDC_1);
+			
+		GtnUIFrameworkWebserviceRequest request = new GtnUIFrameworkWebserviceRequest();
+		request.setGtnWsGeneralRequest(generalWSRequest);
+			
+		GtnUIFrameworkWebserviceComboBoxResponse response = 
+				new GtnUIFrameworkWebServiceClient().callGtnWebServiceUrl(
+						GtnWebServiceUrlConstants.GTN_COMMON_GENERAL_SERVICE
+						+ GtnWebServiceUrlConstants.GTN_COMMON_LOAD_COMBO_BOX, request,
+						GtnUIFrameworkGlobalUI.getGtnWsSecurityToken())
+				.getGtnUIFrameworkWebserviceComboBoxResponse();
+			
+		armUdc1ValueList=new ArrayList<>(response.getItemValueList());
+		armUdc1CodeList=new ArrayList<>(response.getItemCodeList());		
+	}
+	
+	private static void initialize() {
+		getValuesForArmUdc1FromHelperTable();
+		createValueCodeHashMap();
+	}
+	
+	public static int getArmUdc1ItemCode(String itemValue) {
+		initialize();
+		String value=checkItemPresentInHashMap(itemValue);
+		for (Entry<Integer, String> entry : codeValueArmUdc1HashMap.entrySet()) {
+            if (entry.getValue().equals(value)) {
+                return entry.getKey();
+            }
+        }
+		return 0;
+	}
+	
+	public static String getArmUdc1ItemValue(int itemCode) {
+		initialize();
+		return (codeValueArmUdc1HashMap.get(itemCode)==null)?"":codeValueArmUdc1HashMap.get(itemCode);
+	}
+
+	public static List<String> getArmUdc1ValueList() {
+		initialize();
+		return Collections.unmodifiableList(armUdc1ValueList);
+	}
+	
+	public static List<String> getArmUdc1CodeList() {
+		initialize();
+		return Collections.unmodifiableList(armUdc1CodeList);
+	}
+	
+	private static String checkItemPresentInHashMap(String itemValue) {
+		List<String> itemValueArray=
+				Arrays.asList(itemValue.replaceAll(GtnFrameworkItemMasterStringContants.REGEX_SPACE
+						,GtnFrameworkItemMasterStringContants.EMPTY)
+						.split(GtnFrameworkItemMasterStringContants.COMMA));
+		
+		for(String armUdc1Value: armUdc1ValueList) {
+			List<String> armUdc1ValueArray=
+					Arrays.asList(armUdc1Value.replaceAll(GtnFrameworkItemMasterStringContants.REGEX_SPACE
+							,GtnFrameworkItemMasterStringContants.EMPTY)
+					.split(GtnFrameworkItemMasterStringContants.COMMA));
+			
+			if((armUdc1ValueArray.size()==itemValueArray.size()) && (armUdc1ValueArray.containsAll(itemValueArray))) {
+					gtnLogger.info("Equal");
+					return armUdc1Value;
+			}
+		}
+		return GtnFrameworkItemMasterStringContants.EMPTY;
+	}
+}
