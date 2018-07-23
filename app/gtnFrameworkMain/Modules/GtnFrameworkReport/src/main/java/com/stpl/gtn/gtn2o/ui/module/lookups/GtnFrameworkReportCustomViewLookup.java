5d51b975d4ecbf05bcb123704ccdfa439e152ae8
SYSBIZ\Karthik.Raja <karthik.raja@sysbiz.com>
2018-07-23 19:12:52 +0530

    Resolved ALG-5123,ALG-5127

--- app/gtnFrameworkMain/Modules/GtnFrameworkReport/src/main/java/com/stpl/gtn/gtn2o/ui/module/lookups/GtnFrameworkReportCustomViewLookup.java
+++ app/gtnFrameworkMain/Modules/GtnFrameworkReport/src/main/java/com/stpl/gtn/gtn2o/ui/module/lookups/GtnFrameworkReportCustomViewLookup.java
@@ -638,11 +638,17 @@ public class GtnFrameworkReportCustomViewLookup {
 		selectButtonConfig.setParentComponentId(layoutConfig.getComponentId());
 		selectButtonConfig.setAddToParent(true);
 		componentList.add(selectButtonConfig);
+                
+                
+		GtnUIFrameWorkActionConfig closeConfig = new GtnUIFrameWorkActionConfig(
+				GtnUIFrameworkActionType.POPUP_CLOSE_ACTION);
+	
 
 		GtnUIFrameWorkActionConfig selectButtonActionConfig = new GtnUIFrameWorkActionConfig(
 				GtnUIFrameworkActionType.CUSTOM_ACTION);
 		selectButtonActionConfig.addActionParameter(GtnFrameworkUICustomSelectAction.class.getName());
 		selectButtonConfig.addGtnUIFrameWorkActionConfig(selectButtonActionConfig);
+		selectButtonConfig.addGtnUIFrameWorkActionConfig(closeConfig);
 
 		GtnUIFrameworkComponentConfig closeButtonConfig = new GtnUIFrameworkComponentConfig();
 		closeButtonConfig.setComponentType(GtnUIFrameworkComponentType.BUTTON);
@@ -650,11 +656,10 @@ public class GtnFrameworkReportCustomViewLookup {
 		closeButtonConfig.setComponentName("CLOSE");
 		closeButtonConfig.setParentComponentId(layoutConfig.getComponentId());
 		closeButtonConfig.setAddToParent(true);
-
-		GtnUIFrameWorkActionConfig closeConfig = new GtnUIFrameWorkActionConfig(
-				GtnUIFrameworkActionType.POPUP_CLOSE_ACTION);
-		closeButtonConfig.addGtnUIFrameWorkActionConfig(closeConfig);
+	
+                closeButtonConfig.addGtnUIFrameWorkActionConfig(closeConfig);
 		componentList.add(closeButtonConfig);
+                
 
 	}
 
