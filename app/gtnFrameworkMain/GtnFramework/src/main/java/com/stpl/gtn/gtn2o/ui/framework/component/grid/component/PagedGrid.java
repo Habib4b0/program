362ed3189287dda2de748f75dd3acb21926364aa
Aravindasamy <aravindasamy.viswa@sysbiz.com>
2018-07-26 17:51:02 +0530

    ALG-5178,ALG-5177,ALG-5147,ALG-5179 Issue Fixed

--- app/gtnFrameworkMain/GtnFramework/src/main/java/com/stpl/gtn/gtn2o/ui/framework/component/grid/component/PagedGrid.java
+++ app/gtnFrameworkMain/GtnFramework/src/main/java/com/stpl/gtn/gtn2o/ui/framework/component/grid/component/PagedGrid.java
@@ -342,12 +342,11 @@ public class PagedGrid {
         for (Map.Entry<String, Object> entry : tableConfig.getFilterValueMap().entrySet()) {
             String key = getDBColumnName(entry.getKey());
             Object value = entry.getValue();
-            filter.append(condition + " " + key + "  like '%" + value + "%'");
+            filter.append(condition).append(" ").append(key).append("  like '%").append(value).append("%'");
         }
 
         return query.replace("@filter", filter.toString());
     }
-
     private String getDBColumnName(String key) {
         HashMap<String, String> dbColumnMap = new HashMap<>();
         dbColumnMap.put("hierName", "c.HIERARCHY_NAME");
