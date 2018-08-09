aef0f2999266589607505a8b91355b96fbf9f7c7
Gokul Kumar <gokulkumar.murugesan@sysbiz.com>
2018-08-09 11:58:25 +0530

    commit service registry updated changes.

--- app/gtnFrameworkMain/WebServices/GtnServiceRegistryWebServices/src/main/java/com/stpl/gtn/gtn2o/serviceregistry/webservices/GtnUIServiceRegistryService.java
+++ app/gtnFrameworkMain/WebServices/GtnServiceRegistryWebServices/src/main/java/com/stpl/gtn/gtn2o/serviceregistry/webservices/GtnUIServiceRegistryService.java
@@ -1,11 +1,27 @@
 package com.stpl.gtn.gtn2o.serviceregistry.webservices;
 
+import com.stpl.gtn.gtn2o.serviceregistry.bean.GtnWsServiceRegistryBean;
+import com.stpl.gtn.gtn2o.ws.GtnUIFrameworkWebServiceClient;
+import com.stpl.gtn.gtn2o.ws.bean.GtnWsSecurityToken;
 import com.stpl.gtn.gtn2o.ws.request.GtnUIFrameworkWebserviceRequest;
 import com.stpl.gtn.gtn2o.ws.response.GtnUIFrameworkWebserviceResponse;
 
 public class GtnUIServiceRegistryService {
 
 	public GtnUIFrameworkWebserviceResponse serviceRegistryUIServiceCallingWs(GtnUIFrameworkWebserviceRequest request){
-		return new GtnUIFrameworkWebserviceResponse();
+		
+		GtnWsServiceRegistryBean serviceRegistryBean = request.getGtnServiceRegistryWsRequest().getGtnWsServiceRegistryBean();
+		
+		GtnUIFrameworkWebserviceResponse response = new GtnUIFrameworkWebServiceClient().callGtnWebServiceUrl(serviceRegistryBean.getUrl(), serviceRegistryBean.getModuleName(), request, getGsnWsSecurityToken(
+				request.getGtnWsGeneralRequest().getUserId(),request.getGtnWsGeneralRequest().getSessionId()));
+		
+		return response;
+	}
+	
+	private GtnWsSecurityToken getGsnWsSecurityToken(String userId, String sessionId) {
+		GtnWsSecurityToken wsToken = new GtnWsSecurityToken();
+		wsToken.setUserId(userId);
+		wsToken.setSessionId(sessionId);
+		return wsToken;
 	}
 }
--- app/gtnFrameworkMain/WebServices/GtnServiceRegistryWebServices/src/main/java/com/stpl/gtn/gtn2o/serviceregistry/webservices/GtnValidateWsServiceRegistryService.java
+++ app/gtnFrameworkMain/WebServices/GtnServiceRegistryWebServices/src/main/java/com/stpl/gtn/gtn2o/serviceregistry/webservices/GtnValidateWsServiceRegistryService.java
@@ -1,17 +1,54 @@
 package com.stpl.gtn.gtn2o.serviceregistry.webservices;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+import com.stpl.dependency.queryengine.GtnFrameworkQueryExecutorBean;
+import com.stpl.dependency.queryengine.GtnWsQueryType;
+import com.stpl.dependency.queryengine.request.GtnQueryEngineWebServiceRequest;
+import com.stpl.dependency.queryengine.response.GtnQueryEngineWebServiceResponse;
+import com.stpl.gtn.gtn2o.datatype.GtnFrameworkDataType;
 import com.stpl.gtn.gtn2o.serviceregistry.bean.GtnWsServiceRegistryBean;
-import com.stpl.gtn.gtn2o.ws.GtnUIFrameworkWebServiceClient;
-import com.stpl.gtn.gtn2o.ws.response.GtnUIFrameworkWebserviceResponse;
+import com.stpl.gtn.gtn2o.serviceregistry.constants.GtnWsServiceRegistryConstants;
+import com.stpl.gtn.gtn2o.serviceregistry.controller.GtnUIServiceRegistryController;
+import com.stpl.gtn.gtn2o.ws.GtnFrameworkPropertyManager;
 
 public class GtnValidateWsServiceRegistryService {
 
-	public boolean serviceRegistryServiceToValidateWsIsRegistered(GtnWsServiceRegistryBean gtnWsServiceRegistryBean){
+	@Autowired
+	private GtnUIServiceRegistryController gtnUIServiceRegistryController;
+	
+	public boolean serviceRegistryServiceToValidateWsIsRegistered(GtnWsServiceRegistryBean serviceRegistryBean){
 		
-//		GtnUIFrameworkWebserviceResponse response = new GtnUIFrameworkWebServiceClient().callGtnWebServiceUrl("url", request,getGsnWsSecurityToken(request.getGtnWsGeneralRequest().getUserId(),request.getGtnWsGeneralRequest().getSessionId()));
-//		if(response.getGtnServiceRegistryWSResponse().getGtnWsServiceRegistryBean()gtnWsServiceRegistryBean <=0){
-//			return false;
-//		}
-		return true;
+		GtnQueryEngineWebServiceRequest queryEngineWebServiceRequest = new GtnQueryEngineWebServiceRequest();
+		GtnFrameworkQueryExecutorBean queryExecutorBean = new GtnFrameworkQueryExecutorBean();
+		
+		queryExecutorBean.setSqlQuery(GtnWsServiceRegistryConstants.SELECT_QUERY);
+		queryExecutorBean.setParams(new Object[]{"/gtnServiceRegistry"});
+		queryExecutorBean.setDataType(new GtnFrameworkDataType[]{GtnFrameworkDataType.STRING});
+		queryExecutorBean.setQueryType(GtnWsQueryType.SELECTWITHPARAMS);
+		queryEngineWebServiceRequest.setQueryExecutorBean(queryExecutorBean);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		GtnQueryEngineWebServiceResponse response = restTemplate.postForObject(getWebServiceEndpointBasedOnModule("/executeQuery","queryEngine"), queryEngineWebServiceRequest,
+				GtnQueryEngineWebServiceResponse.class);
+		List resultList = (List) response.getResult();
+		for(int i=0;i<resultList.size();i++){
+			
+			if(((List)resultList.get(i)).contains("/gtnServiceRegistry")){
+				return true;
+			}
+		}
+	
+		return false;
 	}
+	public String getWebServiceEndpointBasedOnModule(String url, String moduleName) {
+		return GtnFrameworkPropertyManager.getProperty("gtn.webservices." + moduleName + ".endPointUrl")
+				+ GtnFrameworkPropertyManager.getProperty("gtn.webservices." + moduleName + ".endPointServiceName")
+				+ url;
+
+	}
+	
 }
