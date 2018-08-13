155b4336c3ba5d680f8b5192aab18bd307c8cbc8
Gokul Kumar <gokulkumar.murugesan@sysbiz.com>
2018-08-13 18:25:35 +0530

    commit callGtnService method changes updated files

--- app/gtnFrameworkMain/WebServices/GtnServiceRegistryWebServices/src/main/java/com/stpl/gtn/gtn2o/serviceregistry/controller/GtnUIServiceRegistryController.java
+++ app/gtnFrameworkMain/WebServices/GtnServiceRegistryWebServices/src/main/java/com/stpl/gtn/gtn2o/serviceregistry/controller/GtnUIServiceRegistryController.java
@@ -5,22 +5,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.stpl.dependency.logger.GtnFrameworkDependencyLogger;
-import com.stpl.dependency.queryengine.bean.GtnFrameworkQueryExecutorBean;
-import com.stpl.dependency.queryengine.request.GtnQueryEngineWebServiceRequest;
-import com.stpl.dependency.queryengine.response.GtnQueryEngineWebServiceResponse;
 import com.stpl.dependency.serviceregistryabstract.GtnServiceRegistryImplClass;
-import com.stpl.gtn.gtn2o.datatype.GtnFrameworkDataType;
-import com.stpl.gtn.gtn2o.serviceregistry.constants.GtnWsServiceRegistryConstants;
+import com.stpl.gtn.gtn2o.serviceregistry.webservices.GtnServiceRegistryRegisterWs;
 import com.stpl.gtn.gtn2o.serviceregistry.webservices.GtnUIServiceRegistryService;
-import com.stpl.gtn.gtn2o.serviceregistry.webservices.GtnWsServiceRegistrySqlService;
 import com.stpl.gtn.gtn2o.ws.GtnFrameworkPropertyManager;
 import com.stpl.gtn.gtn2o.ws.request.GtnUIFrameworkWebserviceRequest;
 import com.stpl.gtn.gtn2o.ws.response.GtnUIFrameworkWebserviceResponse;
 import com.stpl.gtn.gtn2o.ws.response.serviceregistry.GtnServiceRegistryWSResponse;
-import com.stpl.gtn.gtn2o.ws.serviceregistry.bean.GtnWsServiceRegistryBean;
 
 @RestController
 @RequestMapping(value = "/gtnServiceRegistry")
@@ -40,39 +33,21 @@ public class GtnUIServiceRegistryController extends GtnServiceRegistryImplClass 
 	private GtnUIServiceRegistryService gtnUIServiceRegistryService;
 
 	@Autowired
-	private GtnWsServiceRegistrySqlService gtnWsServiceRegistrySqlService;
+	private GtnServiceRegistryRegisterWs gtnWsServiceRegistrySqlService;
+	
+	@Autowired
+	private GtnServiceRegistryRegisterWs gtnServiceRegistryRegisterWs;
 
 	@RequestMapping(value = "/registerWebservices", method = RequestMethod.POST)
 	public void registerWebServices(@RequestBody GtnUIFrameworkWebserviceRequest request) {
 
-		GtnQueryEngineWebServiceRequest queryEngineWebServiceRequest = new GtnQueryEngineWebServiceRequest();
-		GtnFrameworkQueryExecutorBean queryExecutorBean = new GtnFrameworkQueryExecutorBean();
-
-		GtnWsServiceRegistryBean serviceRegistryBean = request.getGtnServiceRegistryWsRequest()
-				.getGtnWsServiceRegistryBean();
-
-		Object[] params = new Object[4];
-		params[0] = serviceRegistryBean.getRegisteredWebContext();
-		params[1] = serviceRegistryBean.getHostName();
-		params[2] = serviceRegistryBean.getPort();
-		params[3] = serviceRegistryBean.getRegisteredWebContext();
-
-		GtnFrameworkDataType[] dataType = { GtnFrameworkDataType.STRING, GtnFrameworkDataType.STRING, GtnFrameworkDataType.STRING,
-				GtnFrameworkDataType.STRING };
-
-		String registerQuery = GtnWsServiceRegistryConstants.INSERT_QUERY;
-
-		queryExecutorBean.setParams(params);
-		queryExecutorBean.setDataType(dataType);
-		queryExecutorBean.setSqlQuery(registerQuery);
-		queryExecutorBean.setQueryType("INSERTORUPDATEWITHPARAMS");
-
-		queryEngineWebServiceRequest.setQueryExecutorBean(queryExecutorBean);
-
-		RestTemplate restTemplate = new RestTemplate();
-		GtnQueryEngineWebServiceResponse response = restTemplate.postForObject(
-				getWebServiceEndpointBasedOnModule("/executeQuery", "queryEngine"), queryEngineWebServiceRequest,
-				GtnQueryEngineWebServiceResponse.class);
+		GtnFrameworkDependencyLogger logger = gtnUIServiceRegistryController
+				.logInformation(GtnUIServiceRegistryController.class);
+		logger.debug("inside registerWebservices");
+		
+		gtnServiceRegistryRegisterWs.serviceRegistryRegisterWebServices(request);
+		
+		logger.error("webservices registered");
 	}
 
 	@RequestMapping(value = "/serviceRegistryUIControllerMappingWs", method = RequestMethod.POST)
