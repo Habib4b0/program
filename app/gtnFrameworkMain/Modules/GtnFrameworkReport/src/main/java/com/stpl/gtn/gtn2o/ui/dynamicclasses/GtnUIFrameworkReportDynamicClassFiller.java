d34f0ba14903114cb3d1eda37e7f353de35a9366
Aman Verma <aman.verma@sysbiz.com>
2018-08-16 21:20:28 +0530

    ALG-5438: Issue Fixed.

--- app/gtnFrameworkMain/Modules/GtnFrameworkReport/src/main/java/com/stpl/gtn/gtn2o/ui/dynamicclasses/GtnUIFrameworkReportDynamicClassFiller.java
+++ app/gtnFrameworkMain/Modules/GtnFrameworkReport/src/main/java/com/stpl/gtn/gtn2o/ui/dynamicclasses/GtnUIFrameworkReportDynamicClassFiller.java
@@ -22,6 +22,7 @@ import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportCustomViewDeleteAction;
 import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportCustomViewEditAction;
 import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportDashBoardRightHeaderRequestAction;
 import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportDataSelectionRegenerateConfirmationAction;
+import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportOptionsViewOptionsAction;
 import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportResetAndCloseAction;
 import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportingComparisonOptionsGroupValuesLoadingAction;
 import com.stpl.gtn.gtn2o.ui.action.GtnFrameworkReportingDashboardConfirmUpdateProfileAction;
@@ -305,6 +306,8 @@ public class GtnUIFrameworkReportDynamicClassFiller implements GtnUIDynamicObjec
 				new GtnFrameworkGridToBarChartAction());
 		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkPublicViewDeleteValidation.class.getName(),
 				new GtnFrameworkPublicViewDeleteValidation());
+		GtnUIFrameworkGlobalUI.addDynamicClassObjects(GtnFrameworkReportOptionsViewOptionsAction.class.getName(),
+				new GtnFrameworkReportOptionsViewOptionsAction());
 
 	}
 
