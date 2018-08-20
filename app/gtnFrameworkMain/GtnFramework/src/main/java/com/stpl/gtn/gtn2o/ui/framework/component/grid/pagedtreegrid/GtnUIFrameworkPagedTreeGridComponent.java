72bf56482ae79879337d38388b2f8fd48d6d4ee6
Spandan Majumder <spandan.majumder@sysbiz.com>
2018-08-20 18:07:52 +0530

    ALG-4248 Commit package structure changes

--- app/gtnFrameworkMain/GtnFramework/src/main/java/com/stpl/gtn/gtn2o/ui/framework/component/grid/pagedtreegrid/GtnUIFrameworkPagedTreeGridComponent.java
+++ app/gtnFrameworkMain/GtnFramework/src/main/java/com/stpl/gtn/gtn2o/ui/framework/component/grid/pagedtreegrid/GtnUIFrameworkPagedTreeGridComponent.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.stpl.gtn.gtn2o.ui.framework.component.grid.pagedtreeGrid;
+package com.stpl.gtn.gtn2o.ui.framework.component.grid.pagedtreegrid;
 
 import java.util.ArrayList;
 import java.util.Arrays;
