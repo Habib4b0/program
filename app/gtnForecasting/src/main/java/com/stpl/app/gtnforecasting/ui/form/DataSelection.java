a8decf00f5be7fa1bf95436cdca28141434aa57c
SYSBIZ\Rohit.Vignesh <rohit.vignesh@sysbiz.com>
2018-08-21 16:44:37 +0530

    Sonarqube : 1. Correctness - ExecutorService field doesn't ever get shutdown

--- app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/ui/form/DataSelection.java
+++ app/gtnForecasting/src/main/java/com/stpl/app/gtnforecasting/ui/form/DataSelection.java
@@ -122,7 +122,6 @@ public class DataSelection extends ForecastDataSelection {
 	
 	public static final String CONFIRM_CHANGE = "Confirm Change";
 
-	private final ExecutorService service = ThreadPool.getInstance().getService();
 
 	// Used for CCP_HIERARCHY_INSERT query formation
 	private String topLevelName = StringUtils.EMPTY;
@@ -4483,6 +4482,7 @@ public class DataSelection extends ForecastDataSelection {
 	 * @throws ClassNotFoundException
 	 */
 	private void updateDataSelectionChanges() throws ClassNotFoundException, IOException {
+              final ExecutorService service = ThreadPool.getInstance().getService();
 		dsLogic.deleteFromTempCCPTable(session);
 		relationLogic.ccpHierarchyInsert(session.getCurrentTableNames(), selectedCustomerContainer.getItemIds(),
 				selectedProductContainer.getItemIds(), selectionDTO);
@@ -4514,6 +4514,7 @@ public class DataSelection extends ForecastDataSelection {
                                         dsLogic.getRelationshipDetailsCustom(session, String.valueOf(session.getCustomRelationShipSid())));
                 session.setDiscountCustomerProductLevelDetails(
                                         dsLogic.getRelationshipDetailsCustom(session, String.valueOf(session.getCustomDeductionRelationShipSid())));
+                service.shutdown();
 
 	}
 
