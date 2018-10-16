9f79de18a4d17252f7b39b29442be40c70d1dc5c
HaziHabibullahSyed <hazihabibullah.syed@sysbiz.com>
2018-10-16 14:18:59 +0530

    ALG-6185

---
+++ app/gtnFrameworkMain/WebServices/GTNWebServices/src/test/java/com/stpl/gtn/gtn2o/ws/module/processscheduler/service/GtnWsProcessSchedulerRunValidationServiceTest.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stpl.gtn.gtn2o.ws.module.processscheduler.service;
+
+import com.stpl.gtn.gtn2o.queryengine.engine.GtnFrameworkSqlQueryEngine;
+import com.stpl.gtn.gtn2o.ws.exception.GtnFrameworkGeneralException;
+import com.stpl.gtn.gtn2o.ws.module.automaticrelationship.service.GtnFrameworkAutomaticService;
+import com.stpl.gtn.gtn2o.ws.service.GtnWsSqlService;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author Hazihabibullah.Syed
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/test/resources/AutomaticContext.xml" })
+public class GtnWsProcessSchedulerRunValidationServiceTest {
+    
+    public GtnWsProcessSchedulerRunValidationServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @InjectMocks
+    @Autowired
+    private GtnWsProcessSchedulerRunValidationService gtnWsProcessSchedulerRunValidationService;
+    
+    @Autowired
+    private GtnFrameworkAutomaticService automaticRelationService;
+    
+    @Autowired
+    private GtnWsSqlService gtnWsSqlService;
+    
+    @Mock
+    @Autowired
+    private GtnFrameworkSqlQueryEngine gtnSqlQueryEngine;
+    
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+    /**
+     * Test of getSessionFactory method, of class GtnWsProcessSchedulerRunValidationService.
+     */
+    @Test
+    public void testGetSessionFactory() {
+        System.out.println("getSessionFactory");
+        SessionFactory result = gtnWsProcessSchedulerRunValidationService.getSessionFactory();
+        assertFalse(result==null);
+    }
+
+    /**
+     * Test of validateProcessRun method, of class GtnWsProcessSchedulerRunValidationService.
+     */
+    @Test
+    public void testValidateProcessRun() throws Exception {
+        System.out.println("validateProcessRun");
+        List<String> validationCriteria = new ArrayList<>();
+        validationCriteria.add("value");
+        validationCriteria.add("COMPANY_MASTER_INTERFACE");
+        List<Object> inputlist = new ArrayList<>();
+        inputlist.add("COMPANY_MASTER_INTERFACE");
+        boolean result = gtnWsProcessSchedulerRunValidationService.validateProcessRun(validationCriteria, inputlist);
+        assertFalse(inputlist.isEmpty());
+    }
+    
+    @Test
+    public void testValidateProcessRunIf() throws Exception {
+        System.out.println("validateProcessRun");
+        List<String> validationCriteria = new ArrayList<>();
+        validationCriteria.add("value");
+        validationCriteria.add("value");
+        List<Object> inputlist = new ArrayList<>();
+        inputlist.add("Value");
+        try{
+        boolean result = gtnWsProcessSchedulerRunValidationService.validateProcessRun(validationCriteria, inputlist);
+        assertFalse(inputlist.isEmpty());
+        //Assert.fail();
+        } catch (IndexOutOfBoundsException e) {
+			
+	}
+
+    }
+    
+}
